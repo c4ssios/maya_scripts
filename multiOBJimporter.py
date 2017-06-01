0710570eebc55838f0a3136b7f368a06d6caaaa9
c4ssios <c4ssios@gmail.com>
2017-06-01 15:49:06 +0100

    Update multiOBJimporter.py

--- multiOBJimporter.py
+++ multiOBJimporter.py
@@ -1,3 +1,8 @@
+import maya.cmds as cmds
+import sys
+import os
+
+
 def multiOBJimporter():
    
     objFilter = "*.obj"
@@ -11,3 +16,16 @@ def multiOBJimporter():
         objName = fileName.split('.')
       
         cmds.file(elem, i=True, typ='OBJ', ra=True, mergeNamespacesOnClash=False, namespace=objName[0], options="mo=0")
+
+
+#Recursive import
+
+path = '/job/checkmate/reference/character/Strong/model/DRZ/20151222_Strong_All_Poses'
+fileString = 'RenderMesh.obj'
+
+for dirName, subdirList, fileList in os.walk(path):
+    for filename in fileList:
+        if 'RenderMesh.obj' in filename:
+            p=os.path.join(dirName,filename)
+           
+            cmds.file(filename, i=True, typ='OBJ', ra=True, mergeNamespacesOnClash=False, namespace=objName[0], options="mo=0")
