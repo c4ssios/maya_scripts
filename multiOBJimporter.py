e57eb5cbb63b43cc16cc6592d08460fc1da4e899
c4ssios <c4ssios@gmail.com>
2017-06-01 15:51:21 +0100

    Update multiOBJimporter.py

--- multiOBJimporter.py
+++ multiOBJimporter.py
@@ -18,14 +18,17 @@ def multiOBJimporter():
         cmds.file(elem, i=True, typ='OBJ', ra=True, mergeNamespacesOnClash=False, namespace=objName[0], options="mo=0")
 
 
-#Recursive import
 
+
+################## Recursive import #########################
 path = '/job/checkmate/reference/character/Strong/model/DRZ/20151222_Strong_All_Poses'
 fileString = 'RenderMesh.obj'
+############################################################
 
 for dirName, subdirList, fileList in os.walk(path):
     for filename in fileList:
         if 'RenderMesh.obj' in filename:
             p=os.path.join(dirName,filename)
+            print 'importing ' + p
            
-            cmds.file(filename, i=True, typ='OBJ', ra=True, mergeNamespacesOnClash=False, namespace=objName[0], options="mo=0")
+            cmds.file(p, i=True, typ='OBJ', ra=True, mergeNamespacesOnClash=False, namespace=filename, options="mo=0")
