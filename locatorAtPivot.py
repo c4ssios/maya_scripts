9a470634506d8ccadaac191040ab4c07dfac43b4
c4ssios <c4ssios@gmail.com>
2017-10-04 10:36:18 +0100

    Update locatorAtPivot.py

--- locatorAtPivot.py
+++ locatorAtPivot.py
@@ -1,7 +1,7 @@
 import maya.cmds as cmds
 
 def locatorAtPivot():
-    sel = cmds.ls(sl=True)
+    sel = cmds.ls(sl=True, l=True)
     if len(sel)==0:
         cmds.warning('Select at least on object.')
     else:
@@ -9,7 +9,7 @@ def locatorAtPivot():
             targetPos = cmds.xform(s, q=True, rp=True, worldSpace=True)
             targetRot = cmds.xform(s, q=True, ro=True, worldSpace=True)
             
-            locName = s+'_LOC'
+            locName = s.split('|')[-1] +'_LOC'
                    
             cmds.spaceLocator(n=locName)
             cmds.setAttr(locName+'.translateX', targetPos[0])
@@ -18,4 +18,3 @@ def locatorAtPivot():
             cmds.setAttr(locName+'.rotateX', targetRot[0])
             cmds.setAttr(locName+'.rotateY', targetRot[1])
             cmds.setAttr(locName+'.rotateZ', targetRot[2])
-            
