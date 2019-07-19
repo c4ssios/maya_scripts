7332c157cc555b6609c2c3cbdbe383216c85c612
c4ssios <c4ssios@gmail.com>
2019-07-19 19:12:05 +0200

    Update arnoldPreSurfacing.py

--- arnoldPreSurfacing.py
+++ arnoldPreSurfacing.py
@@ -1,10 +1,14 @@
 import maya.cmds as cmds
 import random
 import re
+import yaml
 
 
 materialList = ['stone','paint','metal','wood','plastic','rubber','cloth','glass','ceramic','skin','hair','nail','bone','liquid','polysterene','leather','default','paper','felt','light', 'concrete', 'plant']
 colorList = ['Grey','Black','White','Yellow','Red','Green','Blue','Orange','Purple','Brown','Pink','Colour','Clear','Mixed']
+
+colorPresetFile = '/servers/Home/nleblanc/pythonScripts/maya/colorPreset.yaml'
+arnoldMaterialPresetFile = '/servers/Home/nleblanc/pythonScripts/maya/arnoldMaterialPreset.yaml'
 
 
 def randomRename():
@@ -22,15 +26,68 @@ def camelCaseSplit(string):
 def filterSelection(selection):
 	offenders = []
 	for obj in selection:
-		if '_' not in obj:
+		objShortName=obj.split('|')[-1]
+		if '_' not in objShortName:
 			offenders.append(obj)
-		elif len(obj.split('_'))!=4:
+		elif len(objShortName.split('_'))!=4:
 			offenders.append(obj)
-		elif len(camelCaseSplit(obj.split('_')[0]))!=2:
+		elif len(camelCaseSplit(objShortName.split('_')[0]))!=2:
 			offenders.append(obj)
-		elif camelCaseSplit(obj.split('_')[0])[0] not in materialList:
+		elif camelCaseSplit(objShortName.split('_')[0])[0] not in materialList:
 			offenders.append(obj)
-		elif camelCaseSplit(obj.split('_')[0])[1] not in colorList:
+		elif camelCaseSplit(objShortName.split('_')[0])[1] not in colorList:
 			offenders.append(obj)
 
 	return offenders
+
+
+def preSurfaceObjects():
+
+	objects = cmds.ls('*:',typ='mesh', l=True)
+	offenders = filterSelection(objects)
+
+	if offenders:
+		for o in offenders:
+			objects.remove(o)
+	
+	uniqueMat=[]
+
+	for obj in objects:
+		objShortName=obj.split('|')[-1]
+		objectMaterialName = objShortName.split('_')[0]
+		if objectMaterialName not in uniqueMat:
+			uniqueMat.append(objectMaterialName)
+
+
+	#Arnold Material Creation
+
+	for m in uniqueMat:
+
+		color = camelCaseSplit(m)[1].lower()
+		materialType = camelCaseSplit(m)[0]
+
+		materialName = m + '_MAT'
+
+		if cmds.objExists(materialName):
+			cmds.delete(materialName)
+		if cmds.objExists(materialName+'_SG'):
+			cmds.delete(materialName+'_SG')
+
+
+		material = cmds.shadingNode('aiStandardSurface', asShader=True, n=materialName)
+		materialSG = cmds.sets(name=materialName+'_SG', empty=True, renderable=True, noSurfaceShader=True)
+		cmds.connectAttr(material+'.outColor', materialSG+'.surfaceShader')
+
+
+		colorPreset = open(colorPresetFile, 'r')
+		colorPresetParsed = yaml.load(colorPreset)
+
+		arnoldMaterialPreset = open(arnoldMaterialPresetFile, 'r')
+		arnoldMaterialParsed = yaml.load(arnoldMaterialPreset)
+
+		for attribute in arnoldMaterialParsed[materialType]:
+			#print arnoldMaterialParsed[materialType][attribute]
+			cmds.setAttr(materialName+'.'+)
+			#cmds.setAttr(materialName+'.baseColor', colorPresetParsed[color]['r'], colorPresetParsed[color]['g'], colorPresetParsed[color]['b'],type='double3')
+
+
