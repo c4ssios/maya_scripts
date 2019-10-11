a907b52075909d1978d94782d8adcc1a56effafe
c4ssios <c4ssios@gmail.com>
2019-10-11 14:38:55 +0200

    Update listDuplicateNames.py

--- listDuplicateNames.py
+++ listDuplicateNames.py
@@ -1,5 +1,13 @@
 import maya.cmds as cmds
 
+
+__title__ = "List Duplicate Names"
+__version__ = '0.1'
+__author__ = "Nicolas Leblanc"
+__company__ = "Dwarf Animation"
+__maintainer__ = "Nicolas Leblanc"
+__email__ = "nicolas.leblanc@dwarfanimation.com"
+
 def listDuplicateNames():
     transformList = cmds.ls(type='transform', l=True)
     uniqueNames = []
@@ -12,11 +20,38 @@ def listDuplicateNames():
             if TRsn not in nonUniqueNames:
                 nonUniqueNames.append(TRsn)
     
-    result = ''
     
     if len(nonUniqueNames)==0:
-        result = 'All Good !'
+
+        cmds.confirmDialog( title='Non Unique List', message='All Good !', button=['Close'], defaultButton='Close', cancelButton='Close', dismissString='Close' )
+        
     else:
-        result = 'Those objets are non-Uniques :\n\n'+'\n'.join(nonUniqueNames)
+        
+        listDuplicateNamesResultUI(nonUniqueNames)
             
-    cmds.confirmDialog( title='Non Unique List', message=result, button=['Close'], defaultButton='Close', cancelButton='Close', dismissString='Close' )
+
+def listDuplicateNamesResultUI(list):
+    windowWidth = 500
+    windowHeight = 300
+    
+    if (cmds.window("listDuplicateNames_window", exists=True)):
+        cmds.deleteUI("listDuplicateNames_window")
+
+    window = cmds.window("listDuplicateNames_window", title= __title__+ ' ' +__version__, iconName='listDuplicateNames', width=windowWidth, height=windowHeight)
+    
+    cmds.columnLayout( adjustableColumn=True )
+    cmds.separator( height=20, style='none' )
+    cmds.text(label='Those objects names are not uniques' )
+    cmds.separator( height=20, style='none' )
+    cmds.textScrollList('listDuplicateNames_scrollList')
+    
+    for l in list:
+        cmds.textScrollList('listDuplicateNames_scrollList', e=True, append=l)
+    
+    cmds.separator( height=20, style='double' )
+
+    cmds.button( label='Close', height=30, command=('cmds.deleteUI(\"' + window + '\", window=True)') )
+
+    cmds.setParent(upLevel=True)
+    cmds.window(window, e=True, width=windowWidth, height=windowHeight)
+    cmds.showWindow( window )
